273fb36c9f09438f9a4e1277b7217a50cec48781
kana <k.salon.du.l@gmail.com>
2024-07-20 17:21:48 +0900

    vueのapitなぐ

--- backend/learning-b/resources/js/Pages/Records/Index.vue
+++ backend/learning-b/resources/js/Pages/Records/Index.vue
@@ -1,6 +1,72 @@
 <script setup>
-import AuthenticatedLayout from "../../Layouts/AuthenticatedLayout.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
+import { Inertia } from "@inertiajs/inertia";
+import { ref } from "vue";
+import { useForm } from "@inertiajs/vue3";
+import { Ziggy } from "@/ziggy"; // Ziggyのルートをインポート
+
+// サーバーから渡されるデータをpropsで受け取ります
+const props = defineProps({
+    records: Array,
+});
+console.log(props.records);
+
+const form = useForm({
+    date: "",
+    duration: "",
+    note: "",
+});
+
+const deleteRecord = (id) => {
+    if (confirm("本当に削除しますか？")) {
+        Inertia.delete(route("records.destroy", { id }, Ziggy), {
+            // Ziggyのルートを使用
+            onSuccess: () => {
+                console.log("Record deleted successfully");
+                refreshRecords();
+            },
+        });
+    }
+};
+
+const editingRecord = ref(null);
+const editForm = useForm({
+    date: "",
+    duration: "",
+    note: "",
+});
+
+const editRecord = (record) => {
+    if (record) {
+        editingRecord.value = record.record_id;
+        editForm.value = {
+            date: record.date,
+            duration: record.duration,
+            note: record.note,
+        };
+        console.log("Edit Form:", editForm.value); // デバッグログ
+    }
+};
+
+const saveRecord = () => {
+    Inertia.put(
+        route("records.update", { id: editingRecord.value }, Ziggy),
+        editForm.value, // editForm.value を渡す
+        {
+            onSuccess: () => {
+                console.log("Record updated successfully");
+                editingRecord.value = null;
+                editForm.reset();
+                refreshRecords();
+            },
+        }
+    );
+};
+
+const refreshRecords = () => {
+    Inertia.reload({ only: ["records"] });
+};
 </script>
 
 <template>
@@ -9,10 +75,101 @@ import { Head } from "@inertiajs/vue3";
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                スケジュール
+                学習記録
             </h2>
         </template>
-
-        <div>Hello World!</div>
+        <div>
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th
+                            scope="col"
+                            class="px-[120px] py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            日付
+                        </th>
+                        <th
+                            scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            学習時間
+                        </th>
+                        <th
+                            scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            勉強内容
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    <tr v-for="record in records" :key="record.id">
+                        <td class="px-[120px] py-4 whitespace-nowrap">
+                            <template v-if="editingRecord === record.record_id">
+                                <input
+                                    v-model="editForm.value.date"
+                                    class="form-input"
+                                />
+                            </template>
+                            <template v-else>
+                                {{ record.date }}
+                            </template>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <template v-if="editingRecord === record.record_id">
+                                <input
+                                    v-model="editForm.value.duration"
+                                    class="form-input"
+                                />
+                            </template>
+                            <template v-else>
+                                {{ record.duration }} 時間
+                            </template>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <template v-if="editingRecord === record.record_id">
+                                <input
+                                    v-model="editForm.value.note"
+                                    class="form-input"
+                                />
+                            </template>
+                            <template v-else>
+                                {{ record.note }}
+                            </template>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <template v-if="editingRecord === record.record_id">
+                                <button
+                                    @click="saveRecord(record.record_id)"
+                                    class="text-green-600 hover:text-green-900"
+                                >
+                                    保存
+                                </button>
+                                <button
+                                    @click="editingRecord = null"
+                                    class="text-gray-600 hover:text-gray-900 ml-4"
+                                >
+                                    キャンセル
+                                </button>
+                            </template>
+                            <template v-else>
+                                <button
+                                    @click="editRecord(record)"
+                                    class="text-indigo-600 hover:text-indigo-900"
+                                >
+                                    編集
+                                </button>
+                                <button
+                                    @click="deleteRecord(record.record_id)"
+                                    class="text-red-600 hover:text-red-900 ml-4"
+                                >
+                                    削除
+                                </button>
+                            </template>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </AuthenticatedLayout>
 </template>
